ec2108ea85856c3a4f6cd4dd8e6f97f10901f8e9
Eduardo D. Oliveira <eduardo.jeso@gmail.com>
2018-06-25 17:15:30 -0300

    Estrutura do Grafo
    bla bla bla

--- GRAFO.py
+++ GRAFO.py
@@ -1,40 +1,118 @@
+class Vertice(object):
+    """docstring for Vertice."""
+    def __init__(self, nome):
+        self.nome = nome
+        self.adjacencias = []
+        self.cor = 'branco'
+        self.inicio = 0
+        self.fim = 0
+
+    def addAdjacencia(self, vertice):
+            self.adjacencias.append(vertice)
+
+    def __str__(self):
+        lista = ''
+        for adj in self.adjacencias:
+            lista += '['+ adj.nome + ']'
+        return '{}-->{}'.format(self.nome, lista)
+
+class Aresta(object):
+    """docstring for Aresta."""
+    def __init__(self, origem: Vertice, destino: Vertice, peso: float):
+        self.origem = origem
+        self.destino = destino
+        self.peso = peso
+
+    def __str__(self):
+        return '{}--{}-->{}'.format(self.origem.nome, self.peso, self.destino.nome)
+
 class Grafo(object):
     """docstring for Grafo."""
-    def __init__(self, arq_grafo):
-        with open(arq_grafo) as arquivo:
-            grafo_info = []
-            for linha in arquivo:
-                grafo_info.append(linha.split())
-            dirigido  = int(grafo_info[0][0])
-            ponderado = int(grafo_info[1][0])
-            tamanho = int(grafo_info[2][0])
-            self.matriz = [0] * tamanho
-            for i in range(tamanho):
-                self.matriz[i] = [0] * tamanho
-        grafo_info = grafo_info[3:]
-        if dirigido:
-            for info in grafo_info:
-                self.inserir(int(info[i][0]),
-                             int(info[i][1]),
-                             dirigido=dirigido)
-        else:
-            for info in grafo_info:
-                self.inserir(int(info[i][0]),
-                             int(info[i][1]),
-                             float(info[i][2]),
-                             dirigido=dirigido)
+    def __init__(self, numero_de_vertices, tipo ):
+        self.arestas = []
+        self.vertices = []
+        self.tipo = tipo
+        self.numero_de_vertices = numero_de_vertices
+        self.matriz = None
 
+    def addAresta(self, nome_origem: Vertice, nome_destino: Vertice, peso: float=1):
+        origem = self.getVertice(nome_origem)
+        if origem is None:
+            origem = Vertice(nome_origem)
 
-    def inserir(self, origem, destino, peso=1, dirigido=0):
-        self.matriz[origem][destino] = peso
-        if not dirigido:
-            self.matriz[destino][origem] = peso
+        destino = self.getVertice(nome_destino)
+        if destino is None:
+            destino = Vertice(nome_destino)
 
-    def showMatriz(self):
-        for i in range(len(self.matriz)):
-            for j in range(self.matriz[i]):
-                print(self.matriz[i][j])
+        e = Aresta(origem, destino, peso)
+        origem.addAdjacencia(destino)
+        self.arestas.append(e)
+        if self.tipo != 'dirigido':
+            print('entrou')
+            a = Aresta(destino, origem, peso)
+            destino.addAdjacencia(origem)
+            self.arestas.append(a)
+        return True
+
+    def addVertice(self, nome: str):
+        if nome not in self.vertices:
+            vertice = Vertice(nome)
+            self.vertices.append(vertice)
+        return True
+
+    def getVertice(self, nome):
+        for v in self.vertices:
+            if nome == v.nome:
+                return v
+        return None
+
+    def lista_arestas(self):
+        for e in self.arestas:
+            print(e)
+
+    def lista_vertices(self):
+        for v in self.vertices:
+            print(v)
+
+    def lista_de_adjacencias(self):
+        lista = ''
+        for v in self.vertices:
+            lista += v.nome + ' -> '
+            for e in self.arestas:
+                if v == e.origem:
+                    lista += '({}, {}) '.format(e.destino.nome, str(e.peso))
+            lista += '\n'
+        return lista
+
+    def show_matriz(self):
+        if self.matriz is None:
+            self.matriz = []
+            self.matriz = [[0]*self.numero_de_vertices for _ in range(self.numero_de_vertices)]
+            for linha in self.matriz:
+                linha = [0] * self.numero_de_vertices
+            for v in self.vertices:
+                for e in self.arestas:
+                    if v.nome == e.origem.nome:
+                        self.matriz[int(v.nome)][int(e.destino.nome)] = e.peso
+            for linha in self.matriz:
+                print(linha)
+
+    def busca_profundidade(self, nome_vertice):
+        return 'Arvore'
+
+    def busca_largura(self, nome_vertice):
+        return 'Arvore'
+
+    def getTransposto(self):
+        for e in self.arestas:
+            e.origem, e.destino = e.destino, e.origem
+
 
 if __name__ == '__main__':
-    g = Grafo('grafo-a.txt')
-    g.showMatriz()
+    g = Grafo(5, tipo='não dirigido')
+    g.addVertice('4')
+    g.addVertice('3')
+    g.addAresta('4', '3', 0.56)
+    # g.addAresta('3', '4', 0.5)
+    print(g.lista_de_adjacencias())
+    g.show_matriz()
